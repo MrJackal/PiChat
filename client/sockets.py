e261ce8067cacbac78d3d2b6f13fb93f3dee85a1
= <jayden.balm@creativeintersection.com>
2017-04-13 14:34:49 +1000

    Adding commands, Adding Handler for when client disconnects, Starting http side

--- client/sockets.py
+++ client/sockets.py
@@ -1,35 +1,139 @@
 import socket
 import sys
 import time
-import traceback
+import threading
 import json
+import requests
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 12345
+apiURL = "/api"
+TCP_PORT = 12346
 BUFFER_SIZE = 1024
 running = True
 
+s = ""
+Username = ""
+_cmdprefix = "!"
 
-while running == True:
+_helpcommands = {
+    "help": {
+        "desc": "A list of all commands.",
+        "cli_cmd": "get_help"
+    }
+}
+
+_commands = {
+    "list": {
+        "name": "list",
+        "desc": "Will return all online users.",
+        "usage": "!list",
+        "srv_cmd": "get_userlist"
+    },
+    "quit": {
+        "name": "quit",
+        "desc": "This will disconnect your from the server.",
+        "usage": "!quit",
+        "srv_cmd": "user_disconnect"
+    },
+    "msg": {
+        "name": "msg",
+        "desc": "Send a private message to the specified person",
+        "usage": "!msg [user]",
+        "srv_cmd": "send_privatemessage"
+    },
+    "ban": {
+        "name": "ban",
+        "desc": "Ban the specified person from the server or Ban them for a certain time e.g. 1m, 3h, 4d, 5w, 6M, 7Y.",
+        "usage": "!ban [user] {time}",
+        "srv_cmd": "user_ban"
+    },
+    "kick": {
+        "name": "kick",
+        "desc": "Kick the specified person from the server.",
+        "usage": "!kick [user]",
+        "srv_cmd": "user_kick"
+    },
+    "mute": {
+        "name": "mute",
+        "desc": "Mute the specified person or Mute then for a certain time e.g. 1m, 3h, 4d, 5w, 6M, 7Y.",
+        "usage": "!mute [user] {time}",
+        "srv_cmd": "user_mute"
+    }
+}
+
+
+def gethelpcommandlist():
+    r = requests.get(TCP_IP+apiURL+"/commands/help")
+    if r.status_code == 200:
+        _commands = r.json()
+
+
+def getcommandlist():
+    r = requests.get(TCP_IP+apiURL+"/commands/all")
+    if r.status_code == 200:
+        _commands = r.json()
+
+
+def client(conn):
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+        if not data:
+            break
+
+        response_data = json.loads(data)
+        if response_data["cmd"] == "recv_message":
+            print(response_data["data"]["username"] + ": " + response_data["data"]["message"])
+
+        if response_data["cmd"] == "recv_connection":
+            print(response_data["data"]["username"] + " has joined.")
+
+
+def get_help():
+    print "----- Displaying Results for Help -----\n"
+    for command in _commands.values():
+        printed = 0
+        for command_details in command.keys():
+            if printed == 0:
+                print command["name"] + ":- " + command["desc"] + " Usage: " + command["usage"] + "\n"
+                printed = 1
+    print "----- End Results for Help -----\n"
+
+
+def send_userconnect(sock, username):
+    send_connection = {'cmd': 'user_connect', 'data': {'username': username}}
+    sock.send(json.dumps(send_connection))
+
+
+def sendmessage(sock):
+    while True:
+        MESSAGE = (raw_input())
+        if MESSAGE == "!exit":
+            sock.close()
+            raise SystemExit
+        if MESSAGE == _cmdprefix + "help":
+            get_help()
+
+        message_data = {'cmd': 'send_message', 'data': {'username': Username, 'message': MESSAGE}}
+
+        sock.send(json.dumps(message_data))
+
+
+while running:
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("Connecting...")
-        s.connect((TCP_IP,TCP_PORT))
+        s.connect((TCP_IP, TCP_PORT))
+
         Username = (raw_input("Username: "))
-        print("Connected!")
-        while True:
-            MESSAGE = (raw_input("Message: "))
-            if MESSAGE == "exit":
-                s.close()
-                raise SystemExit
-            message_data = {'username': Username, 'message': MESSAGE}
+        print(Username + " has joined")
 
-            s.send(json.dumps(message_data))
-            data = s.recv(BUFFER_SIZE)
+        send_userconnect(s, Username)
 
-            response_data = json.loads(data)
+        # Store thread for receiving from server
+        threading.Thread(target=client, args=(s,)).start()
 
-            print(response_data.username + ": " + response_data.message)
+        # Store thread for sending messages
+        threading.Thread(target=sendmessage, args=(s,)).start()
 
         running = False
         time.sleep(20)
--- server/sockets_server.py
+++ server/sockets_server.py
@@ -1,19 +1,34 @@
-import socket
-import time
-import sys
-import threading
 import json
+import socket
+import threading
+import requests
 
 from clients import ChatClient
 
-
 TCP_IP = '127.0.0.1'
-TCP_PORT = 12345
+TCP_PORT = 12346
 BUFFER_SIZE = 1024
 
-CLIENTS = {}
+
+# Description/Message Templates
+# <[X]> used to replace with an integer
+# <[L]> used to replace with a list of strings/ints/doubles etc
 
 _clientList = []
+
+_validcommands = {
+    "help": {},
+    "list": {},
+    "quit": {},
+    "msg": {},
+    "ban": {},
+    "kick": {},
+    "mute": {}
+}
+
+_requestcommands = {
+    "get_userlist"  # Returns list of users
+}
 
 
 def client(conn):
@@ -21,30 +36,47 @@ def client(conn):
         data = conn.recv(BUFFER_SIZE)
         if not data:
             break
-        # broadcast
-        # for client in CLIENTS:
-        #    client.send(data)
+
         _userdata = {}
         for chatClient in _clientList:
             _userdata = json.loads(data)
-            print(_userdata)
-            chatClient.conn.send(data)
+            username = _userdata["data"]["username"]
+            if _userdata["cmd"] == "user_connect":
+                msg = {"cmd": "recv_connection", "data": {"username": username}}
+                chatClient.conn.send(json.dumps(msg))
+                print "[INFO] %s Connected" % (_userdata["data"]["username"])
+            if _userdata["cmd"] == "send_message":
+                chatClient.conn.send(data)
+                print "[INFO] %s: %s" % (_userdata["data"]["username"], _userdata["data"]["message"])
 
-        print "[INFO] %s: %s" % (_userdata.username, _userdata.message)
-    # the connection is closed: unregister
-    del CLIENTS[conn.fileno()]
+    for index, chatClient in enumerate(_clientList):
+        if chatClient.conn.fileno() == conn.fileno:
+            del _clientList[index]
+
+
+def commands(conn):
+    while True:
+        data = conn
 
 
 def listener():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((TCP_IP,TCP_PORT))
+    s.bind((TCP_IP, TCP_PORT))
     s.listen(5)
     while True:
         conn, addr = s.accept()
         # register client
-        CLIENTS[conn.fileno()] = conn
-        _clientList.append(ChatClient(conn,"",1))
+        _clientList.append(ChatClient(conn, "", 1))
+
+        # Thread new clients and their sockets
         threading.Thread(target=client, args=(conn,)).start()
+
+        # Thread console commands
+        threading.Thread(target=commands, args=(conn,)).start()
+
+    # server closed connection - stop the socket
+    s.close()
+
 
 if __name__ == '__main__':
     listener()
